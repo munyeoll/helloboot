774cd8814185b473a89ef3ddaf636c0c3fafc14b
munyeoll <munyeol08@gmail.com>
2023-05-05 21:49:14 +0900

    Test : 섹션 3, 서블릿 컨테이너 띄우기

--- src/main/java/tobyspring/helloboot/HellospringApplication.java
+++ src/main/java/tobyspring/helloboot/HellospringApplication.java
@@ -1,12 +1,15 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class HellospringApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Containerless Standalone Application");
-	}
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
+    }
 
 }
