e0574f30cf665924a18bd7189f1f693380e2600a
munyeoll <munyeol08@gmail.com>
2023-05-05 21:24:21 +0900

    First commit

---
+++ src/main/java/tobyspring/helloboot/HellospringApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringApplication.class, args);
+	}
+
+}
