07f370ad347893696c2a21994ea504426abaff98
munyeoll <munyeol08@gmail.com>
2023-05-15 23:00:23 +0900

    섹션 4, @ComponentScan

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,11 +2,12 @@ package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RequestMapping("/hello")
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
@@ -16,7 +17,7 @@ public class HelloController {
     }
 
     @GetMapping
-    @ResponseBody
+//    @ResponseBody @RestController 에서 @ResponseBody 를 메타 애노테이션으로 가지고 있음
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
