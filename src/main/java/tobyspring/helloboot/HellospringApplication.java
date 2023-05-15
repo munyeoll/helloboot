9bb828cc266c9089613a35012ef386396a1a25e6
munyeoll <munyeol08@gmail.com>
2023-05-15 23:24:38 +0900

    섹션 4, SpringBootApplication

--- src/main/java/tobyspring/helloboot/HellospringApplication.java
+++ src/main/java/tobyspring/helloboot/HellospringApplication.java
@@ -1,12 +1,11 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,26 +23,7 @@ public class HellospringApplication {
     }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                // DispatcherServlet 은 ApplicationContextAware 을 구현하였기 때문에 자동으로 ApplicationContext 가 주입됨
-                // 따라서 하단 로직은 필요 없음
-                // dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(HellospringApplication.class);
-        applicationContext.refresh();
+        SpringApplication.run(HellospringApplication.class, args);
     }
 
 }
