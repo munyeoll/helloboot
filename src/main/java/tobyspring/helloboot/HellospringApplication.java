d160588d56ec93bc5dea7c85a03c01252e12ab11
munyeoll <munyeol08@gmail.com>
2023-06-01 20:22:42 +0900

    섹션 8, 자동구성에 Environment Property 적용

--- src/main/java/tobyspring/helloboot/HellospringApplication.java
+++ src/main/java/tobyspring/helloboot/HellospringApplication.java
@@ -1,10 +1,21 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HellospringApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            String property = environment.getProperty("my.name");
+            System.out.println("my.name: " + property);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellospringApplication.class, args);
--- src/main/java/tobyspring/helloboot/WebServerConfiguration.java
+++ src/main/java/tobyspring/helloboot/WebServerConfiguration.java
@@ -2,12 +2,10 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 public class WebServerConfiguration {
-    @Bean
+    //@Bean
     public ServletWebServerFactory customerWebServerFactory() {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         serverFactory.setPort(9090);
