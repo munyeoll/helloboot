35b322ba3ae40fbe7378cbf64718b0ed100fb7c8
munyeoll <munyeol08@gmail.com>
2023-06-12 21:05:27 +0900

    섹션 10, 스프링부트의 자동구성과 테스트로 전환

--- src/main/java/tobyspring/helloboot/HellospringApplication.java
+++ src/main/java/tobyspring/helloboot/HellospringApplication.java
@@ -1,28 +1,17 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
-import tobyspring.config.MySpringBootApplication;
 
 import javax.annotation.PostConstruct;
 
-@MySpringBootApplication
+@SpringBootApplication
 public class HellospringApplication {
     private final JdbcTemplate jdbcTemplate;
 
     public HellospringApplication(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
-    }
-
-    @Bean
-    ApplicationRunner applicationRunner(Environment environment) {
-        return args -> {
-            String property = environment.getProperty("my.name");
-            System.out.println("my.name: " + property);
-        };
     }
 
     @PostConstruct
--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -2,14 +2,15 @@ package tobyspring.helloboot;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class HelloApiTest {
-
     @Test
     void helloApi() {
         // http -v GET ":8080/hello?name=String"
